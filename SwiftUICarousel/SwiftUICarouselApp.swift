121a162c065cbb4d8dced70696be67e26280fdb4
EngEmadHany <emadhany2007@gmail.com>
2023-08-03 00:09:31 +0400

    Initial Commit

---
+++ SwiftUICarousel/SwiftUICarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICarouselApp.swift
+//  SwiftUICarousel
+//
+//  Created by Emad Isaac on 17/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
